84be7e6fec995187e6534bcf2164575da430ddcf
GuidoCeresa <gac@algos.it>
2014-06-30 11:54:13 +0200

    copyFile and moveFile

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -13,7 +13,7 @@ String appDir = "grails-app/"
 String confDir = "${appDir}conf/"
 
 // copy readme into project
-moveFile(source, dest, "${appDir}README", "README-Logo")
+copyFile(source, dest, "${appDir}README", "README-Logo")
 print('------------')
 print('Algoslogo - creato (o sovrascritto) README-Logo')
 print('------------')
@@ -24,8 +24,20 @@ print('------------')
 print('Algoslogo - creato (NON sovrascritto) LogoBootStrap')
 print('------------')
 
+public static copyFile(String srcDirPath, String dstDirPath, String fileName) {
+    copyFile(srcDirPath, dstDirPath, fileName, fileName)
+} // fine del metodo
+
 public static moveFile(String srcDirPath, String dstDirPath, String fileName) {
     moveFile(srcDirPath, dstDirPath, fileName, fileName)
+} // fine del metodo
+
+public static copyFile(String srcDirPath, String dstDirPath, String srcFileName, String dstFileName) {
+    String srcFile = srcDirPath + srcFileName
+    String destFile = dstDirPath + dstFileName
+
+    copyFile(srcFile, destFile)
+    deleteFile(srcFile)
 } // fine del metodo
 
 public static moveFile(String srcDirPath, String dstDirPath, String srcFileName, String dstFileName) {
@@ -39,5 +51,6 @@ public static copyFile(String srcFile, String destFile) {
     new AntBuilder().copy(file: srcFile, tofile: destFile, overwrite: true)
 } // fine del metodo
 
-
-
+public static deleteFile(String pathFile) {
+    new AntBuilder().delete(file: pathFile)
+} // fine del metodo
