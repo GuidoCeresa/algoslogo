927c6a141bcfed06ceceb0bd40399a8949f68bb9
GuidoCeresa <gac@algos.it>
2015-07-10 07:48:39 +0200

    0.32

--- grails-app/controllers/it/algos/algoslogo/LogoController.groovy
+++ grails-app/controllers/it/algos/algoslogo/LogoController.groovy
@@ -10,7 +10,7 @@ class LogoController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
+        params.max = Math.min(max ?: 20, 100)
         respond Logo.list(params), model: [logoInstanceCount: Logo.count()]
     }
 
