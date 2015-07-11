2aa3e2a18e77f0206c641cfdd93aa36538d614e2
GuidoCeresa <gac@algos.it>
2015-07-11 08:13:15 +0200

    0.33

--- AlgoslogoGrailsPlugin.groovy
+++ AlgoslogoGrailsPlugin.groovy
@@ -3,7 +3,7 @@
 //
 class AlgoslogoGrailsPlugin {
     // the plugin version
-    def version = "0.32"
+    def version = "0.33"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4.2 > *"
     // the other plugins this plugin depends on
